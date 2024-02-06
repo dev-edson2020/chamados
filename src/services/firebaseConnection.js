50dd329bb28ac15b8401511460e1338d8d46002d
Edson Meira <edson.meira@itixti.com.br>
2024-02-06 16:50:55 -0300

    Correção conexão firebase

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -5,14 +5,15 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBioQFfkwb2-CMk-8eoklLOG_pGdDGkSEs",
-    authDomain: "tickets-1ab7c.firebaseapp.com",
-    projectId: "tickets-1ab7c",
-    storageBucket: "tickets-1ab7c.appspot.com",
-    messagingSenderId: "839953219621",
-    appId: "1:839953219621:web:72996b326211cb9e167fcc",
-    measurementId: "G-KRS4N2KF15"
-  };
+  apiKey: "AIzaSyD5w_vfzNkYi6bvfZ50pAtJkmmo7rZMjbc",
+  authDomain: "tickets-5348f.firebaseapp.com",
+  databaseURL: "https://tickets-5348f-default-rtdb.firebaseio.com",
+  projectId: "tickets-5348f",
+  storageBucket: "tickets-5348f.appspot.com",
+  messagingSenderId: "723047905406",
+  appId: "1:723047905406:web:f123ca6f44fd3148e3402f",
+  measurementId: "G-XS3DLWEBSC"
+};
 
   const firebaseApp = initializeApp(firebaseConfig);
   const auth = getAuth(firebaseApp);
