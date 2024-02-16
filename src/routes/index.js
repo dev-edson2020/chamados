7426a15da2fd25e1165c38c760f16391ccf4724b
Edson Meira <edson.meira@itixti.com.br>
2024-02-16 17:26:17 -0300

    Página clientes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,8 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard'
 import Profile from '../pages/Profile';
-
 import Private from './Private'
+import Customers from '../pages/Customers';
 
 function RoutesApp() {
   return (
@@ -15,6 +15,7 @@ function RoutesApp() {
 
       <Route path="/dashboard" element={<Private><Dashboard/></Private>} />
       <Route path="/profile" element={<Private><Profile/></Private>} />
+      <Route path="/customers" element={<Private><Customers/></Private>} />
     </Routes>
   )
 }
